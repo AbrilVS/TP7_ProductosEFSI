6692b2a4f52f185b8122deec5981c1983f0495bf
ABRIL VARELA SICA <48464274@ort>
2025-06-26 09:08:31 -0300

    algo

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,30 +1,30 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import Card from '../components/Card';
+// import axios from 'axios';
+// import React, { useEffect, useState } from 'react'
+// import Card from '../components/Card';
 
 
-function Productos() {
-const [productos,setProductos] = useState([]);
-    useEffect(()=>{
-        //llamar api
-        axios.get('https://fakestoreapi.com/products').then(response=>{
-            console.log(response)
-            setProductos(response.data);
-        })        
-    },[])
+// function Productos() {
+// const [productos,setProductos] = useState([]);
+//     useEffect(()=>{
+//         //llamar api
+//         axios.get('https://fakestoreapi.com/products').then(response=>{
+//             console.log(response)
+//             setProductos(response.data);
+//         })        
+//     },[])
 
-  return (
-    <>
-      <h2>Productos</h2>
-      <div className="cards-container">
-        {productos.map(item => (
-          <Card key={item.id} item={item} />
-        ))}
-      </div>
-    </>
+//   return (
+//     <>
+//       <h2>Productos</h2>
+//       <div className="cards-container">
+//         {productos.map(item => (
+//           <Card key={item.id} item={item} />
+//         ))}
+//       </div>
+//     </>
     
-  )
+//   )
   
-}
+// }
 
-export default Productos
+// export default Productos
